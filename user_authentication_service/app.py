2cadefe07d0ed384c384de72f18543768adf025b
Nkennyelvis <e.nsengiman@alustudent.com>
2026-09-25 15:35:53 +0200

    User authentication service

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -11,9 +11,9 @@ app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def hello_world():
-    """hello world"""
-    return jsonify({"message": "Hello World"})
+def Bienvenue():
+    """Bienvenue"""
+    return jsonify({"message": "Bienvenue"})
 
 
 @app.route('/users', methods=['POST'])
